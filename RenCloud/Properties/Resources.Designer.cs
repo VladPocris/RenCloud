81a635441bf27e8136ca9ad7cf7cce3fca416d21
Vlad Pocris <X00202628@MyTuDublin.ie>
2024-09-26 12:49:27 +0100

    SmoothBackgroundGif

--- RenCloud/Properties/Resources.Designer.cs
+++ RenCloud/Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace RenCloud.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Background {
+            get {
+                object obj = ResourceManager.GetObject("Background", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Close {
             get {
                 object obj = ResourceManager.GetObject("Close", resourceCulture);
