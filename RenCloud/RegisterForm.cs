c8e9c54c0bad3369d9683e637c3c111d646d4c5d
Vlad Pocris <X00202628@MyTuDublin.ie>
2024-10-31 08:55:42 +0000

    RegisterForm_V2

--- RenCloud/RegisterForm.cs
+++ RenCloud/RegisterForm.cs
@@ -7,15 +7,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static RenCloud.Program;
 
 namespace RenCloud
 {
     public partial class RegisterForm : Form
     {
+
+        //Variables&Objects
+        private bool isActive = false;
+        private Corners applyCorners;
+        private GifAnimation gifAnimation;
+        private DragFunctionality dragFunctionality;
+
+        //ROUNDCORNERS LOGIC//
+        protected override void OnActivated(EventArgs e)
+        {
+            base.OnActivated(e);
+            isActive = false;
+            applyCorners.AttributesRoundCorners(this, isActive);
+        }
+        protected override void OnDeactivate(EventArgs e)
+        {
+            base.OnDeactivate(e);
+            isActive = true;
+            applyCorners.AttributesRoundCorners(this, isActive);
+        }
         public RegisterForm()
         {
             InitializeComponent();
+            //ENABLE DOUBLE BUFFER//
+            this.DoubleBuffered = true;
+            //GIF ANIMATION INIT//
+            gifAnimation = new GifAnimation(pictureBox12);
+            gifAnimation.InitializeGifAnimation(Properties.Resources.NetworkconnectionBackgroundHDDarkGeometricAbstractBackdrop_ezgif_com_speed);
+            //APPLY ROUND CORNERS//
+            applyCorners = new Corners();
+            //APPLY DRAGGING FUNCTIONALITY//
+            dragFunctionality = new DragFunctionality();
+            //START FROM CENTER//
             this.StartPosition = FormStartPosition.CenterScreen;
+            this.Shown += RegisterForm_Shown;
+            this.Load += RegisterForm_Load;
+        }
+
+        private void RegisterForm_Load(object sender, EventArgs e)
+        {
+            //ATTACHMENTS AND ON-LOAD FEATURES//
+            applyCorners.AttributesRoundCorners(this, isActive);
+            dragFunctionality.AttachDraggingEvent(panel1, this);
+            dragFunctionality.AttachDraggingEvent(label1, this);
+        }
+
+        private void RegisterForm_Shown(Object sender, EventArgs e)
+        {
+            tbemail.Focus();
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
@@ -29,6 +75,16 @@ namespace RenCloud
         }
 
         private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox12_Click(object sender, EventArgs e)
         {
 
         }
