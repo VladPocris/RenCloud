2e5591f05b69d9587bdd208bec5052bbc49080e9
Vlad Pocris <X00202628@MyTuDublin.ie>
2024-11-07 14:42:57 +0000

    FormManager&BugFixes

---
+++ RenCloud/PassRes_Form.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static RenCloud.FormManager;
+
+namespace RenCloud
+{
+    public partial class PassRes_Form : Form
+    {
+        public PassRes_Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
