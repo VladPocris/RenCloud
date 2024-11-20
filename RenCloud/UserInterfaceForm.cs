db47975e941b6ff81aea34206dc14dc74051db07
Vlad Pocris <vpocris@gmail.com>
2024-11-20 17:17:14 +0000

    UIDesign_Stage1

---
+++ RenCloud/UserInterfaceForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RenCloud
+{
+    public partial class UserInterfaceForm : Form
+    {
+        public UserInterfaceForm()
+        {
+            InitializeComponent();
+        }
+
+        private void UserInterfaceForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
